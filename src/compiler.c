fd1038c63e517631eb4a131b3daae891293ee794
Pan <kpkong@hk01.com>
2024-09-18 00:43:31 +0800

    fix: struct and function position
    feat: up to 23.4
    
    fix: function position
    
    feat: ch 24 is difficult to follow with code fragments, finish the code first
    
    feat: up to 25.4

--- src/compiler.c
+++ src/compiler.c
@@ -42,13 +42,44 @@ typedef struct
   Precedence precedence;
 } ParseRule;
 
+typedef struct
+{
+  Token name;
+  int depth;
+  bool isCaptured;
+} Local;
+
+typedef struct
+{
+  uint8_t index;
+  bool isLocal;
+} Upvalue;
+
+typedef enum
+{
+  TYPE_FUNCTION,
+  TYPE_SCRIPT
+} FunctionType;
+
+typedef struct Compiler
+{
+  struct Compiler *enclosing;
+  ObjFunction *function;
+  FunctionType type;
+
+  Local locals[UINT8_COUNT];
+  int localCount;
+  Upvalue upvalues[UINT8_COUNT];
+  int scopeDepth;
+} Compiler;
+
 Parser parser;
 Compiler *current = NULL;
 Chunk *compilingChunk;
 
 static Chunk *currentChunk()
 {
-  return compilingChunk;
+  return &current->function->chunk;
 }
 
 static void errorAt(Token *token, const char *message)
@@ -137,8 +168,30 @@ static void emitBytes(uint8_t byte1, uint8_t byte2)
   emitByte(byte2);
 }
 
+static void emitLoop(int loopStart)
+{
+  emitByte(OP_LOOP);
+
+  int offset = currentChunk()->count - loopStart + 2;
+  if (offset > UINT16_MAX)
+    error("Loop body too large.");
+
+  emitByte((offset >> 8) & 0xff);
+  emitByte(offset & 0xff);
+}
+
+static int emitJump(uint8_t instruction)
+{
+  emitByte(instruction);
+  emitByte(0xff);
+  emitByte(0xff);
+
+  return currentChunk()->count - 2;
+}
+
 static void emitReturn()
 {
+  emitByte(OP_NIL);
   emitByte(OP_RETURN);
 }
 
@@ -159,23 +212,60 @@ static void emitConstant(Value value)
   emitBytes(OP_CONSTANT, makeConstant(value));
 }
 
-static void initCompiler(Compiler *compiler)
+static void patchJump(int offset)
 {
-  compiler->localCount = 0;
-  compiler->scopeDepth = 0;
-  current = compiler;
+  // -2 to adjust for the bytecode for the jump offset itself.
+  int jump = currentChunk()->count - offset - 2;
+
+  if (jump > UINT16_MAX)
+  {
+    error("Too much code to jump over.");
+  }
+
+  currentChunk()->code[offset] = (jump >> 8) & 0xff;
+  currentChunk()->code[offset + 1] = jump & 0xff;
 }
 
-static void endCompiler()
+static void initCompiler(Compiler *compiler, FunctionType type)
+{
+  compiler->enclosing = current;
+  compiler->function = NULL;
+  compiler->type = type;
+  compiler->localCount = 0;
+  compiler->scopeDepth = 0;
+  compiler->function = newFunction();
+  current = compiler;
+
+  if (type != TYPE_SCRIPT)
+  {
+    current->function->name = copyString(parser.previous.start,
+                                         parser.previous.length);
+  }
+
+  Local *local = &current->locals[current->localCount++];
+  local->depth = 0;
+  local->isCaptured = false;
+  local->name.start = "";
+  local->name.length = 0;
+}
+
+static ObjFunction *endCompiler()
 {
   emitReturn();
+  ObjFunction *function = current->function;
 
 #ifdef DEBUG_PRINT_CODE
   if (!parser.hadError)
   {
-    disassembleChunk(currentChunk(), "code");
+    disassembleChunk(currentChunk(), function->name != NULL
+                                         ? function->name->chars
+                                         : "<script>");
   }
 #endif
+
+  current = current->enclosing;
+
+  return function;
 }
 
 static void beginScope()
@@ -191,7 +281,14 @@ static void endScope()
          current->locals[current->localCount - 1].depth >
              current->scopeDepth)
   {
-    emitByte(OP_POP);
+    if (current->locals[current->localCount - 1].isCaptured)
+    {
+      emitByte(OP_CLOSE_UPVALUE);
+    }
+    else
+    {
+      emitByte(OP_POP);
+    }
     current->localCount--;
   }
 }
@@ -203,6 +300,150 @@ static void statement();
 static void declaration();
 static ParseRule *getRule(TokenType type);
 static void parsePrecedence(Precedence precedence);
+
+static uint8_t identifierConstant(Token *name)
+{
+  return makeConstant(OBJ_VAL(copyString(name->start,
+                                         name->length)));
+}
+
+static bool identifiersEqual(Token *a, Token *b)
+{
+  if (a->length != b->length)
+    return false;
+
+  return memcmp(a->start, b->start, a->length) == 0;
+}
+
+static int resolveLocal(Compiler *compiler, Token *name)
+{
+  for (int i = compiler->localCount - 1; i >= 0; i--)
+  {
+    Local *local = &compiler->locals[i];
+
+    if (identifiersEqual(name, &local->name))
+    {
+      if (local->depth == -1)
+      {
+        error("Can't read local variable in its own initializer.");
+      }
+      return i;
+    }
+  }
+
+  return -1;
+}
+
+static int addUpvalue(Compiler *compiler, uint8_t index,
+                      bool isLocal)
+{
+  int upvalueCount = compiler->function->upvalueCount;
+  for (int i = 0; i < upvalueCount; i++)
+  {
+    Upvalue *upvalue = &compiler->upvalues[i];
+    if (upvalue->index == index && upvalue->isLocal == isLocal)
+    {
+      return i;
+    }
+  }
+
+  if (upvalueCount == UINT8_COUNT)
+  {
+    error("Too many closure variables in function.");
+    return 0;
+  }
+
+  compiler->upvalues[upvalueCount].isLocal = isLocal;
+  compiler->upvalues[upvalueCount].index = index;
+  return compiler->function->upvalueCount++;
+}
+
+static int resolveUpvalue(Compiler *compiler, Token *name)
+{
+  if (compiler->enclosing == NULL)
+    return -1;
+
+  int local = resolveLocal(compiler->enclosing, name);
+  if (local != -1)
+  {
+    compiler->enclosing->locals[local].isCaptured = true;
+    return addUpvalue(compiler, (uint8_t)local, true);
+  }
+
+  int upvalue = resolveUpvalue(compiler->enclosing, name);
+  if (upvalue != -1)
+  {
+    return addUpvalue(compiler, (uint8_t)upvalue, false);
+  }
+
+  return -1;
+}
+
+static void addLocal(Token name)
+{
+  if (current->localCount == UINT8_COUNT)
+  {
+    error("Too many local variables in function.");
+    return;
+  }
+
+  Local *local = &current->locals[current->localCount++];
+  local->name = name;
+  local->depth = -1;
+  local->isCaptured = false;
+}
+
+static void declareVariable()
+{
+  if (current->scopeDepth == 0)
+    return;
+
+  Token *name = &parser.previous;
+  for (int i = current->localCount - 1; i >= 0; i--)
+  {
+    Local *local = &current->locals[i];
+    if (local->depth != -1 && local->depth < current->scopeDepth)
+    {
+      break;
+    }
+
+    if (identifiersEqual(name, &local->name))
+    {
+      error("Already a variable with this name in this scope.");
+    }
+  }
+
+  addLocal(*name);
+}
+
+static uint8_t argumentList()
+{
+  uint8_t argCount = 0;
+  if (!check(TOKEN_RIGHT_PAREN))
+  {
+    do
+    {
+      expression();
+      if (argCount == 255)
+      {
+        error("Can't have more than 255 arguments.");
+      }
+      argCount++;
+    } while (match(TOKEN_COMMA));
+  }
+  consume(TOKEN_RIGHT_PAREN, "Expect ')' after arguments.");
+  return argCount;
+}
+
+static void and_(bool canAssign)
+{
+  int endJump = emitJump(OP_JUMP_IF_FALSE);
+
+  emitByte(OP_POP);
+  parsePrecedence(PREC_AND);
+
+  patchJump(endJump);
+}
 
 static void binary(bool canAssign)
 {
@@ -247,6 +488,12 @@ static void binary(bool canAssign)
   }
 }
 
+static void call(bool canAssign)
+{
+  uint8_t argCount = argumentList();
+  emitBytes(OP_CALL, argCount);
+}
+
 static void literal(bool canAssign)
 {
   switch (parser.previous.type)
@@ -277,6 +524,18 @@ static void number(bool canAssign)
   emitConstant(NUMBER_VAL(value));
 }
 
+static void or_(bool canAssign)
+{
+  int elseJump = emitJump(OP_JUMP_IF_FALSE);
+  int endJump = emitJump(OP_JUMP);
+
+  patchJump(elseJump);
+  emitByte(OP_POP);
+
+  parsePrecedence(PREC_OR);
+  patchJump(endJump);
+}
+
 static void string(bool canAssign)
 {
   emitConstant(OBJ_VAL(copyString(parser.previous.start + 1,
@@ -292,6 +551,11 @@ static void namedVariable(Token name, bool canAssign)
   {
     getOp = OP_GET_LOCAL;
     setOp = OP_SET_LOCAL;
+  }
+  else if ((arg = resolveUpvalue(current, &name)) != -1)
+  {
+    getOp = OP_GET_UPVALUE;
+    setOp = OP_SET_UPVALUE;
   }
   else
   {
@@ -338,7 +602,7 @@ static void unary(bool canAssign)
 }
 
 ParseRule rules[] = {
-    [TOKEN_LEFT_PAREN] = {grouping, NULL, PREC_NONE},
+    [TOKEN_LEFT_PAREN] = {grouping, call, PREC_CALL},
     [TOKEN_RIGHT_PAREN] = {NULL, NULL, PREC_NONE},
     [TOKEN_LEFT_BRACE] = {NULL, NULL, PREC_NONE},
     [TOKEN_RIGHT_BRACE] = {NULL, NULL, PREC_NONE},
@@ -360,7 +624,7 @@ ParseRule rules[] = {
     [TOKEN_IDENTIFIER] = {variable, NULL, PREC_NONE},
     [TOKEN_STRING] = {string, NULL, PREC_NONE},
     [TOKEN_NUMBER] = {number, NULL, PREC_NONE},
-    [TOKEN_AND] = {NULL, NULL, PREC_NONE},
+    [TOKEN_AND] = {NULL, and_, PREC_AND},
     [TOKEN_CLASS] = {NULL, NULL, PREC_NONE},
     [TOKEN_ELSE] = {NULL, NULL, PREC_NONE},
     [TOKEN_FALSE] = {literal, NULL, PREC_NONE},
@@ -368,7 +632,7 @@ ParseRule rules[] = {
     [TOKEN_FUN] = {NULL, NULL, PREC_NONE},
     [TOKEN_IF] = {NULL, NULL, PREC_NONE},
     [TOKEN_NIL] = {literal, NULL, PREC_NONE},
-    [TOKEN_OR] = {NULL, NULL, PREC_NONE},
+    [TOKEN_OR] = {NULL, or_, PREC_OR},
     [TOKEN_PRINT] = {NULL, NULL, PREC_NONE},
     [TOKEN_RETURN] = {NULL, NULL, PREC_NONE},
     [TOKEN_SUPER] = {NULL, NULL, PREC_NONE},
@@ -379,19 +643,6 @@ ParseRule rules[] = {
     [TOKEN_ERROR] = {NULL, NULL, PREC_NONE},
     [TOKEN_EOF] = {NULL, NULL, PREC_NONE},
 };
-
-typedef struct
-{
-  Token name;
-  int depth;
-} Local;
-
-typedef struct
-{
-  Local locals[UINT8_COUNT];
-  int localCount;
-  int scopeDepth;
-} Compiler;
 
 static void parsePrecedence(Precedence precedence)
 {
@@ -421,75 +672,6 @@ static void parsePrecedence(Precedence precedence)
   }
 }
 
-static uint8_t identifierConstant(Token *name)
-{
-  return makeConstant(OBJ_VAL(copyString(name->start,
-                                         name->length)));
-}
-
-static bool identifiersEqual(Token *a, Token *b)
-{
-  if (a->length != b->length)
-    return false;
-
-  return memcmp(a->start, b->start, a->length) == 0;
-}
-
-static int resolveLocal(Compiler *compiler, Token *name)
-{
-  for (int i = compiler->localCount - 1; i >= 0; i--)
-  {
-    Local *local = &compiler->locals[i];
-
-    if (identifiersEqual(name, &local->name))
-    {
-      if (local->depth == -1)
-      {
-        error("Can't read local variable in its own initializer.");
-      }
-      return i;
-    }
-  }
-
-  return -1;
-}
-
-static void addLocal(Token name)
-{
-  if (current->localCount == UINT8_COUNT)
-  {
-    error("Too many local variables in function.");
-    return;
-  }
-
-  Local *local = &current->locals[current->localCount++];
-  local->name = name;
-  local->depth = -1;
-}
-
-static void declareVariable()
-{
-  if (current->scopeDepth == 0)
-    return;
-
-  Token *name = &parser.previous;
-  for (int i = current->localCount - 1; i >= 0; i--)
-  {
-    Local *local = &current->locals[i];
-    if (local->depth != -1 && local->depth < current->scopeDepth)
-    {
-      break;
-    }
-
-    if (identifiersEqual(name, &local->name))
-    {
-      error("Already a variable with this name in this scope.");
-    }
-  }
-
-  addLocal(*name);
-}
-
 static uint8_t parseVariable(const char *errorMessage)
 {
   consume(TOKEN_IDENTIFIER, errorMessage);
@@ -504,6 +686,9 @@ static uint8_t parseVariable(const char *errorMessage)
 
 static void markInitialized()
 {
+  if (current->scopeDepth == 0)
+    return;
+
   current->locals[current->localCount - 1].depth =
       current->scopeDepth;
 }
@@ -539,6 +724,50 @@ static void block()
   consume(TOKEN_RIGHT_BRACE, "Expect '}' after block.");
 }
 
+static void function(FunctionType type)
+{
+  Compiler compiler;
+  initCompiler(&compiler, type);
+  beginScope();
+
+  consume(TOKEN_LEFT_PAREN, "Expect '(' after function name.");
+
+  if (!check(TOKEN_RIGHT_PAREN))
+  {
+    do
+    {
+      current->function->arity++;
+      if (current->function->arity > 255)
+      {
+        errorAtCurrent("Can't have more than 255 parameters.");
+      }
+      uint8_t constant = parseVariable("Expect parameter name.");
+      defineVariable(constant);
+    } while (match(TOKEN_COMMA));
+  }
+
+  consume(TOKEN_RIGHT_PAREN, "Expect ')' after parameters.");
+  consume(TOKEN_LEFT_BRACE, "Expect '{' before function body.");
+  block();
+
+  ObjFunction *function = endCompiler();
+  emitBytes(OP_CLOSURE, makeConstant(OBJ_VAL(function)));
+
+  for (int i = 0; i < function->upvalueCount; i++)
+  {
+    emitByte(compiler.upvalues[i].isLocal ? 1 : 0);
+    emitByte(compiler.upvalues[i].index);
+  }
+}
+
+static void funDeclaration()
+{
+  uint8_t global = parseVariable("Expect function name.");
+  markInitialized();
+  function(TYPE_FUNCTION);
+  defineVariable(global);
+}
+
 static void varDeclaration()
 {
   uint8_t global = parseVariable("Expect variable name.");
@@ -564,11 +793,121 @@ static void expressionStatement()
   emitByte(OP_POP);
 }
 
+static void forStatement()
+{
+  beginScope();
+  consume(TOKEN_LEFT_PAREN, "Expect '(' after 'for'.");
+  if (match(TOKEN_SEMICOLON))
+  {
+    // No initializer.
+  }
+  else if (match(TOKEN_VAR))
+  {
+    varDeclaration();
+  }
+  else
+  {
+    expressionStatement();
+  }
+
+  int loopStart = currentChunk()->count;
+  int exitJump = -1;
+  if (!match(TOKEN_SEMICOLON))
+  {
+    expression();
+    consume(TOKEN_SEMICOLON, "Expect ';' after loop condition.");
+
+    // Jump out of the loop if the condition is false.
+    exitJump = emitJump(OP_JUMP_IF_FALSE);
+    emitByte(OP_POP); // Condition.
+  }
+
+  if (!match(TOKEN_RIGHT_PAREN))
+  {
+    int bodyJump = emitJump(OP_JUMP);
+    int incrementStart = currentChunk()->count;
+    expression();
+    emitByte(OP_POP);
+    consume(TOKEN_RIGHT_PAREN, "Expect ')' after for clauses.");
+
+    emitLoop(loopStart);
+    loopStart = incrementStart;
+    patchJump(bodyJump);
+  }
+
+  statement();
+  emitLoop(loopStart);
+
+  if (exitJump != -1)
+  {
+    patchJump(exitJump);
+    emitByte(OP_POP); // Condition.
+  }
+
+  endScope();
+}
+
+static void ifStatement()
+{
+  consume(TOKEN_LEFT_PAREN, "Expect '(' after 'if'.");
+  expression();
+  consume(TOKEN_RIGHT_PAREN, "Expect ')' after condition.");
+
+  int thenJump = emitJump(OP_JUMP_IF_FALSE);
+  emitByte(OP_POP);
+  statement();
+
+  int elseJump = emitJump(OP_JUMP);
+
+  patchJump(thenJump);
+  emitByte(OP_POP);
+
+  if (match(TOKEN_ELSE))
+    statement();
+
+  patchJump(elseJump);
+}
+
 static void printStatement()
 {
   expression();
   consume(TOKEN_SEMICOLON, "Expect ';' after value.");
   emitByte(OP_PRINT);
+}
+
+static void returnStatement()
+{
+  if (current->type == TYPE_SCRIPT)
+  {
+    error("Can't return from top-level code.");
+  }
+
+  if (match(TOKEN_SEMICOLON))
+  {
+    emitReturn();
+  }
+  else
+  {
+    expression();
+    consume(TOKEN_SEMICOLON, "Expect ';' after return value.");
+    emitByte(OP_RETURN);
+  }
+}
+
+static void whileStatement()
+{
+  int loopStart = currentChunk()->count;
+  consume(TOKEN_LEFT_PAREN, "Expect '(' after 'while'.");
+  expression();
+  consume(TOKEN_RIGHT_PAREN, "Expect ')' after condition.");
+
+  int exitJump = emitJump(OP_JUMP_IF_FALSE);
+  emitByte(OP_POP);
+  statement();
+  emitLoop(loopStart);
+
+  patchJump(exitJump);
+  emitByte(OP_POP);
 }
 
 static void synchronize()
@@ -600,7 +939,11 @@ static void synchronize()
 
 static void declaration()
 {
-  if (match(TOKEN_VAR))
+  if (match(TOKEN_FUN))
+  {
+    funDeclaration();
+  }
+  else if (match(TOKEN_VAR))
   {
     varDeclaration();
   }
@@ -619,6 +962,22 @@ static void statement()
   {
     printStatement();
   }
+  else if (match(TOKEN_FOR))
+  {
+    forStatement();
+  }
+  else if (match(TOKEN_IF))
+  {
+    ifStatement();
+  }
+  else if (match(TOKEN_RETURN))
+  {
+    returnStatement();
+  }
+  else if (match(TOKEN_WHILE))
+  {
+    whileStatement();
+  }
   else if (match(TOKEN_LEFT_BRACE))
   {
     beginScope();
@@ -631,12 +990,11 @@ static void statement()
   }
 }
 
-bool compile(const char *source, Chunk *chunk)
+ObjFunction *compile(const char *source)
 {
   initScanner(source);
   Compiler compiler;
-  initCompiler(&compiler);
-  compilingChunk = chunk;
+  initCompiler(&compiler, TYPE_SCRIPT);
 
   parser.hadError = false;
   parser.panicMode = false;
@@ -648,7 +1006,7 @@ bool compile(const char *source, Chunk *chunk)
     declaration();
   }
 
-  endCompiler();
+  ObjFunction *function = endCompiler();
 
-  return !parser.hadError;
+  return parser.hadError ? NULL : function;
 }
--- src/debug.c
+++ src/debug.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 
 #include "debug.h"
+#include "object.h"
 #include "value.h"
 
 void disassembleChunk(Chunk *chunk, const char *name)
@@ -39,6 +40,17 @@ static int byteInstruction(const char *name,
   printf("%-16s %4d\n", name, slot);
 
   return offset + 2;
+}
+
+static int jumpInstruction(const char *name, int sign,
+                           Chunk *chunk, int offset)
+{
+  uint16_t jump = (uint16_t)(chunk->code[offset + 1] << 8);
+  jump |= chunk->code[offset + 2];
+  printf("%-16s %4d -> %d\n", name, offset,
+         offset + 3 + sign * jump);
+
+  return offset + 3;
 }
 
 int disassembleInstruction(Chunk *chunk, int offset)
@@ -83,6 +95,10 @@ int disassembleInstruction(Chunk *chunk, int offset)
   case OP_SET_GLOBAL:
     return constantInstruction("OP_SET_GLOBAL",
                                chunk, offset);
+  case OP_GET_UPVALUE:
+    return byteInstruction("OP_GET_UPVALUE", chunk, offset);
+  case OP_SET_UPVALUE:
+    return byteInstruction("OP_SET_UPVALUE", chunk, offset);
   case OP_EQUAL:
     return simpleInstruction("OP_EQUAL", offset);
   case OP_GREATER:
@@ -103,6 +119,36 @@ int disassembleInstruction(Chunk *chunk, int offset)
     return simpleInstruction("OP_NEGATE", offset);
   case OP_PRINT:
     return simpleInstruction("OP_PRINT", offset);
+  case OP_JUMP:
+    return jumpInstruction("OP_JUMP", 1, chunk, offset);
+  case OP_JUMP_IF_FALSE:
+    return jumpInstruction("OP_JUMP_IF_FALSE", 1, chunk, offset);
+  case OP_LOOP:
+    return jumpInstruction("OP_LOOP", -1, chunk, offset);
+  case OP_CALL:
+    return byteInstruction("OP_CALL", chunk, offset);
+  case OP_CLOSURE:
+  {
+    offset++;
+    uint8_t constant = chunk->code[offset++];
+    printf("%-16s %4d ", "OP_CLOSURE", constant);
+    printValue(chunk->constants.values[constant]);
+    printf("\n");
+
+    ObjFunction *function = AS_FUNCTION(
+        chunk->constants.values[constant]);
+    for (int j = 0; j < function->upvalueCount; j++)
+    {
+      int isLocal = chunk->code[offset++];
+      int index = chunk->code[offset++];
+      printf("%04d      |                     %s %d\n",
+             offset - 2, isLocal ? "local" : "upvalue", index);
+    }
+
+    return offset;
+  }
+  case OP_CLOSE_UPVALUE:
+    return simpleInstruction("OP_CLOSE_UPVALUE", offset);
   case OP_RETURN:
     return simpleInstruction("OP_RETURN", offset);
   default:
